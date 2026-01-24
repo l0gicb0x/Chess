3135b280faa57caf5a7ad6760758f200de41e60d
l0gicb0x <shriyannandy@gmail.com>
2026-01-25 00:58:35 +0530

    updated the logic further

--- Backend1/src/Game.ts
+++ Backend1/src/Game.ts
@@ -1,29 +1,76 @@
 
 import  {WebSocket} from "ws";
+import { Chess } from 'chess.js'
+import { GAME_OVER, INIT_GAME, MOVE } from "./messages.js";
 
-    export class Game {
-        public player1: WebSocket; 
-        public player2: WebSocket;
-        private board: string;
-        private moves : string[];
-        private startTime: Date;
+export class Game {
+    public player1: WebSocket; 
+    public player2: WebSocket;
+    public board: Chess
+    private startTime: Date;
 
-        constructor(player1: WebSocket, player2: WebSocket){
-            this.player1 = player1;
-            this.player2 = player2;
-            this.board =  "";
-            this.moves = [];
-            this.startTime = new Date();
+    constructor(player1: WebSocket, player2: WebSocket){
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = new Chess();
+        this.startTime = new Date();
+        this.player1.send(JSON.stringify({type: INIT_GAME,
+            payload: {
+                color: 'white'
+    }
+        }));
+        this.player2.send(JSON.stringify({type: INIT_GAME,
+            payload: {
+                color: 'black'
+    }
+        }));
+
+                        }
+
+
+    makeMove(socket: WebSocket, move: {
+        from: string;
+        to: string;
+    }){
+        if(this.board.moves.length %2 ===0 && socket !== this.player1){
+            return;
+        }
+        if(this.board.moves.length %2 === 1 && socket !== this.player2){
+            return;
+        }
+        try{
+            this.board.move(move);
+        }
+        catch(e){
+             
         }
 
+        if(this.board.isGameOver()){
+            this.player1.emit(JSON.stringify({type: GAME_OVER,
+                payload: {
+                    Winner: this.board.turn() === 'w' ? 'black' : 'white',
+                }
 
-        makeMove(socket: WebSocket, move: string){
-            if(socket === this.player1 || socket === this.player2){
-                this.moves.push(move);
-                this.board = move;
-                this.checkWinner();
+            }));
+             this.player2.emit(JSON.stringify({type: GAME_OVER,
+                payload: {
+                    Winner: this.board.turn() === 'w' ? 'black' : 'white',
+                }
+
+            }));
+        }
+
+        if(this.board.moves.length %2 ===0){
+            this.player1.emit(JSON.stringify({type: MOVE,
+                payload:move
+            }));
+            {
+                this.player2.emit(JSON.stringify({type: MOVE,
+                    payload:move
+                }));
             }
+                
+    }
 
-        }
-        
-    } 
+}
+}
--- Backend1/src/messages.ts
+++ Backend1/src/messages.ts
@@ -1,2 +1,3 @@
 export const INIT_GAME = "INIT_GAME";
 export const MOVE = "MOVE";
+export const GAME_OVER = "GAME_OVER";
