84be416fe81a3b402f9d99b587680244c39c1d92
l0gicb0x <shriyannandy@gmail.com>
2026-01-30 02:21:42 +0530

    Started the Frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter basename='/app'>
+      <Routes>
+      <Route path="/"element={<Landing/>}/>
+      <Route path="/game" element={<Game/>}/>
+
+        </Routes> 
+
+      </BrowserRouter>
+      <button>
+        Join Room
+      </button>
+    </>
+  )
+}
+
+export default App
