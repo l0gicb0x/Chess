9ae48fd9214b29d01695b993040b135c375d1460
l0gicb0x <shriyannandy@gmail.com>
2026-03-21 01:34:03 +0530

    Updated the game logic of connecting and making the board visible on the frontend further

--- Frontend/src/Screens/Game.tsx
+++ Frontend/src/Screens/Game.tsx
@@ -1,6 +1,41 @@
 import { ChessBoard } from "../components/ChessBoard"
+import { Button } from "../components/Button"
+import { useSocket } from "../hooks/useSocket";
+import { useEffect, useState } from "react";
+
+
+export const INIT_GAME = "INIT_GAME";
+export const MOVE = "MOVE";
+export const GAME_OVER = "GAME_OVER";
+
 
 export const Game = () => {
+    const socket = useSocket();
+    const [board, setBoard] = useState();
+
+    useEffect(() => {
+        if(!socket) return;
+
+        socket.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+            console.log(message);
+            switch(message.type) {
+                case INIT_GAME:
+                    console.log("Game initialized");
+                    break;
+                case MOVE:
+                    console.log("Move made");
+                    break;
+                case GAME_OVER:
+                    console.log("Game over");
+                    break;
+            }
+
+        }
+    }, [socket]);
+
+    if(!socket)  return <div className="text-white">Connecting to server...</div>
+
     return <div className=" justify-center flex">
         <div className="pt-8 max-w-Screen-lg w-full">
             <div className="grid grid-cols-6 gap-4 w-full ">
@@ -8,7 +43,15 @@ export const Game = () => {
                     <ChessBoard/>
                 </div>
                 <div className="col-span-2 bg-green-200 w-full">
-                    <button  >Play</button>
+                    <Button 
+    onClick={() => {
+        socket.send(JSON.stringify({
+            type: INIT_GAME
+        }))
+    }}
+>
+    Play Online
+</Button>
                 </div>
 
             </div>
--- Frontend/src/Screens/Landing.tsx
+++ Frontend/src/Screens/Landing.tsx
@@ -1,24 +1,26 @@
 import { useNavigate } from "react-router-dom";
+import { Button } from "../components/Button";
 
 export const Landing = () => {
     const navigate = useNavigate();
     return <div className="flex justify-center">
         <div className="pt-8 max-w-Screen-lg">
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 ">
                     <div className="flex justify-center">
                     <img src={"/ChessBoard.jpeg"}
                         alt="Chess Board"
                         className="max-w-96"/>
                 </div>
-                <div className="pt-10">
-
+                <div className="pt-16">
+                <div className="flex justify-center">
                     <h1 className="text-4xl font-bold mb-4 text-white">Play Chess Online on the #2 Site!</h1>
-                    <div className="mt-4 flex justify-center">
-                        <button 
+                </div>
+                    <div className="mt-8 flex justify-center">
+                        <Button 
                         onClick={() => navigate("/game")}
-                        className="px-8 py-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        >
                             Play Online
-                        </button>
+                        </Button>
 
                     </div>
                 </div>
